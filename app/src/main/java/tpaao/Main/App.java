d2affe807d55282f177df5387961707f75c881c6
TheF16 <116013488+TheF16@users.noreply.github.com>
2023-05-19 16:37:31 +0100

    Setup javafx to display network

--- app/src/main/java/tpaao/Main/App.java
+++ app/src/main/java/tpaao/Main/App.java
@@ -3,6 +3,16 @@
  */
 package tpaao.Main;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import tpaao.DataStructure.City;
+import tpaao.DataStructure.Network;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
